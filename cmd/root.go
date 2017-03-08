cc5bb08d05c2f85d6df144560d1cf54c9fa20d06
Philippe Hässig <phil@nine.ch>
2017-03-08 20:05:19 +0100

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,63 @@
+// Copyright © 2017 Philippe Hässig <phil@neckhair.ch>
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "gcron",
+	Short: "Like cron, but for a single user",
+	Long: `gcron runs regular tasks defined in its config file.
+
+It is mainly inteded to be run inside a Docker container and
+designed to be run as an unprivileged user.`,
+
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports Persistent Flags, which, if defined here,
+	// will be global for your application.
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gcron.yaml)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" { // enable ability to specify config file via flag
+		viper.SetConfigFile(cfgFile)
+	}
+
+	viper.SetConfigName(".gcron") // name of config file (without extension)
+	viper.AddConfigPath("$HOME")  // adding home directory as first search path
+	viper.AutomaticEnv()          // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+// Copyright © 2017 Philippe Hässig <phil@neckhair.ch>
+
+package main
+
+import "github.com/neckhair/gcron/cmd"
+
+func main() {
+	cmd.Execute()
+}
